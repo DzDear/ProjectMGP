61cf19a598c85b45fd3e42acd7a2f6cf0a9360c3
Saudear <final.fantasyxii@outlook.com>
2018-12-03 20:00:35 +0700

    first commit

--- resources/views/temperature/export.blade.php
+++ resources/views/temperature/export.blade.php
@@ -31,12 +31,14 @@
         </thead>
 
         <tbody>
-          @foreach($data['title'] as $key_title => $item)
+            @php $sum10 = 0 @endphp
+            @foreach($data['title'] as $key_title => $item)
               <tr align="center">
                 <td>{{ $item['lc'] }}</td>
                 <td>{{ $item['sc'] }}</td>
 
                 @php $count = []; @endphp
+                @php $arrSum = []; @endphp
                 @foreach ($data['data'] as $key => $value)
                   @if ($value['lc'] == $item['lc'] && $value['sc'] == $item['sc'])
                     @php $count[$value['month']] = $value['count']; @endphp
@@ -45,17 +47,43 @@
 
                 @php $arr = array('01','02','03','04','05','06','07','08','09','10', '11','12') @endphp
                 @php $sum = 0 @endphp
+                @php $i = 1 @endphp
+
                 @foreach($arr as $m)
                   @if (array_key_exists($m, $count))
                     @php $sum += $count[$m] @endphp
+
+                    @php $arrSum[] += $count[$m] @endphp
+
+                    @php $sum10 += $count[$m] @endphp
                     <td>{{ $count[$m] }}</td>
                   @else
                     <td></td>
                   @endif
+                  {{-- @php $i = $i + 1 @endphp --}}
                 @endforeach
                 <td>{{ $sum }}</td>
               </tr>
             @endforeach
+
+            <tr align="center">
+              <th class="text-center">รวม</th>
+              <th></th>
+              <th class="text-center"></th>
+              <th class="text-center"></th>
+              <th class="text-center"></th>
+              <th class="text-center"></th>
+              <th class="text-center"></th>
+              <th class="text-center"></th>
+              <th class="text-center"></th>
+              <th class="text-center"></th>
+              <th class="text-center"></th>
+              <th class="text-center"></th>
+              <th class="text-center"></th>
+              <th class="text-center"></th>
+              <th class="text-center"></th>
+            </tr>
+
           </tbody>
         </table>
 
